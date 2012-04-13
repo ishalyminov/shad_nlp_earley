9642fea3944f65d45c30a47d93ed9c25e55bbf57
Igor Shalyminov <ishalyminov@gmail.com>
2012-04-13 15:24:37 +0400

    A/an articles choosing implemented

---
+++ example.py
@@ -0,0 +1,14 @@
+EXAMPLE_FILES = [
+    'examples/task_1_positive',
+    'examples/task_1_negative',
+    'examples/task_2',
+    'examples/task_4_positive',
+    'examples/task_4_negative'
+]
+
+# returns a (header, examples) tuple
+def load_examples(in_file):
+    lines = [line.strip() for line in in_file]
+    if len(lines) > 1:
+        return (lines[0], lines[1:])
+    return (None, None)
