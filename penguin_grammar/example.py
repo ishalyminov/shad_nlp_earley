bbb61d293e4fe95d9eaeee80bb05e0bd5531f1d9
Veronika Ivannikova <veronika.ivannikova@gmail.com>
2012-04-15 23:37:27 +0400

    task3 grammar

---
+++ penguin_grammar/example.py
@@ -0,0 +1,18 @@
+EXAMPLE_FILES = [
+    #'examples/task_1_positive',
+    #'examples/task_1_negative',
+    #'examples/task_2',
+	  'examples/task_3_positive'
+    #'examples/task_4_positive',
+    #'examples/task_4_negative',
+    #'examples/task_5',
+    #'examples/task_7_negative',
+    #'examples/task_7_positive'
+]
+
+# returns a (header, examples) tuple
+def load_examples(in_file):
+    lines = [line.strip() for line in in_file]
+    if len(lines) > 1:
+        return (lines[0], lines[1:])
+    return (None, None)
